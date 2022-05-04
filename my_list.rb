19fde7dda34ef7b51891b083b086c07e5d72df52
Awais Amjed <awaisamjed171@gmail.com>
2022-05-04 17:48:37 +0500

    Add MyEnumerable

--- my_list.rb
+++ my_list.rb
@@ -1,9 +1,13 @@
+require './my_enumerable'
+
 class MyList
-  def initialize(list)
+  include MyEnumerable
+
+  def initialize(*list)
     @list = list
   end
 
-  def self.each(&block)
+  def each(&block)
     return 'No block given' unless block_given?
 
     @list.each(&block)
