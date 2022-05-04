7d99fcffa65b51d99b0ad19454f682b14c2dd4fb
Awais Amjed <awaisamjed171@gmail.com>
2022-05-04 16:04:06 +0500

    Fix string literals

--- my_list.rb
+++ my_list.rb
@@ -2,7 +2,7 @@ class MyList
   list
 
   def self.each
-    return "No block given" unless block_given?
+    return 'No block given' unless block_given?
 
     self.list.each { |value| yield value }
   end
